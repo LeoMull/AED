802648de58598a0633b9cd4813e098be87e9021c
LeoMull <leonardluismuller@gmail.com>
2023-06-18 22:12:18 -0300

    Exercicio alocacao dinamica numero 8

---
+++ progDescomplicada/alocDin_exer8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int *p, cont2=0,cont=0;
+	p = (int *) calloc(1500, sizeof(int));
+
+	for(int i =0; i<1500; i++){
+		if(*(p+i)==0){
+			cont++;	
+		}	
+	cont2++;
+	}
+
+	printf("Número de espaços alocados %d com 0\n", cont);
+	
+	for(int i=0; i<1500; i++){
+		p[i]=i;
+	}
+	for(int i=0; i<10; ++i){
+		printf("Maiores números = %d | Menores números = %d \n", p[1499-i], p[i]);
+	}
+}
